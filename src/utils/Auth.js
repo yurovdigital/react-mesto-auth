352dcf80fd1b6debfd2874ace2c8360582b1dcc5
YD <yurovdigital@gmail.com>
2021-12-17 21:35:10 +0300

    f

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -39,12 +39,12 @@ export const login = (email, password) => {
     })
 }
 
-export const checkToken = (jwt) => {
+export const checkToken = (token) => {
   return fetch(`${BASE_URL}/users/me`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${jwt}`,
+      Authorization: `Bearer ${token}`,
     },
   }).then((res) => hanldeResponse(res))
 }
