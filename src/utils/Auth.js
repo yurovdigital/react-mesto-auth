740e66272c889cb63b0e0eb538e106442f0bd74e
YD <yurovdigital@gmail.com>
2021-12-17 22:44:59 +0300

    fix

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -44,7 +44,7 @@ export const checkToken = (token) => {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
+      Authorization: `Bearer ${token}`,
     },
   }).then((res) => hanldeResponse(res))
 }
